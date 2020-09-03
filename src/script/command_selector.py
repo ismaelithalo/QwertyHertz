749bd4f89509f075cb36e9d09109e9a63969a7bd
ismaelithalo <ismaelithalo@gmail.com>
2020-09-03 09:08:28 -0300

    Comando shutdown linux

--- src/script/command_selector.py
+++ src/script/command_selector.py
@@ -1,6 +1,7 @@
 import json
 import pyautogui
 import time
+import subprocess
 
 with open('src/json/command.json', 'r') as input_file:
     data = json.load(input_file)
@@ -14,6 +15,7 @@ print("\nO comando dito foi: {}".format(inst))
 coman_1 = "alternar"
 coman_2 = "fechar"
 coman_3 = "Abrir terminal"
+coman_4 = "desligar"
 
 if inst == coman_1: 
 	pyautogui.hotkey('alt','tab')
@@ -29,5 +31,9 @@ elif inst == coman_3:
         pyautogui.hotkey('alt','tab')
         time.sleep(2)
         pyautogui.hotkey('ctrl','alt','t')
+elif inst == coman_4: 
+        pyautogui.hotkey('alt','tab')
+        time.sleep(2)
+        subprocess.run(["shutdown", "now"])
 else:
         print("Comando nao listado ...")
