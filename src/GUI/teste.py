36866c61719e7c24e91c90bb5978ecffb051a0c7
ismaelithalo <ismaelithalo@gmail.com>
2020-09-03 16:59:52 -0300

    Mais testes de console in GUI

--- src/GUI/teste.py
+++ src/GUI/teste.py
@@ -1,5 +1,6 @@
 import tkinter as tk 
 import sys
+import subprocess
 
 class PrintLogger(): # create file like object
     def __init__(self, textbox): # pass reference to text widget
@@ -13,26 +14,24 @@ class PrintLogger(): # create file like object
         pass
 
 if __name__ == '__main__':
-    def consol_write_t():
-        print('i did something')
-    def consol_write_y():
-        print('Teste de log')
+    def w_title():
+        print('Olá, bem vindo(a) ao QwertyHertz!')
         #window.after(1000, consol_write)
 
 window = tk.Tk()
 window.title("QwertyHertz")
 
 label = tk.Label(
-    	text="Olá, bem vindo(a) ao QwertyHertz!",
-    	fg="white",  # Set the text color to white
-    	bg="black",  # Set the background color to black
+    	text="QwertyHertz!",
+    	fg="black",  # Set the text color to white
+    	bg="white",  # Set the background color to black
 	    height=5,
 	    width=80
 )
 label.pack()
 
 t = tk.Text(
-    	#text="Olá, bem vindo(a) ao QwertyHertz!",
+    	#text="",
     	fg="white",  # Set the text color to white
     	bg="black",  # Set the background color to black
 	    height=20,
@@ -44,6 +43,6 @@ t.pack()
 pl = PrintLogger(t)
 sys.stdout = pl
 
-window.after(1000, consol_write_t)
-window.after(2000, consol_write_y)
+window.after(1000, w_title)
+#window.after(2000, W_test)
 window.mainloop()
