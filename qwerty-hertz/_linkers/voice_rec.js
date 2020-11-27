2ea47debe12be60704f363c9641742488be5458b
Ismael Ithalo <ismaelithalo@gmail.com>
2020-11-27 10:51:38 -0300

    Removendo linhas inúteis

--- qwerty-hertz/_linkers/voice_rec.js
+++ qwerty-hertz/_linkers/voice_rec.js
@@ -1,16 +1,5 @@
 function voiceRec(){
-    // var {PythonShell} = require('python-shell')
-    // var path = require('path')
 
-    // var opcoes = {
-    //     scriptPath: path.join(__dirname, '../_engine/')
-    // }
-
-    // var rec = new PythonShell('voice_rec.py', opcoes)
-
-    // rec.on("message", function(message){
-    //     alert(message)
-    // })
     var child_process = require('child_process');
     child_process.exec("start cmd.exe /C python3 script/qwert_hertz.py");
     
